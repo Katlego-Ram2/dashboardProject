ca73777fda39890fd40d15aaa6baebc4c4802a80
katlego.rampedi <katlego.rampedi@Ekurhuleni.gov.za>
2023-07-11 08:59:28 +0200

    added divs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DisplayComponent } from './display/display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidebarComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule,
